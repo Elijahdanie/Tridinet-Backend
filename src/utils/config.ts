0ff637665da4c3f49471f69a71a4dda578a6fe46
Elijah Daniel <thachromatone@gmail.com>
2022-06-22 00:23:26 +0100

    integrated mysql2

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,11 +4,11 @@ import {config} from 'dotenv';
 
 config();
 const node_env = process.env.NODE_ENV;
-const sequelize : Sequelize =  (node_env === 'development') ? new Sequelize({
+const sequelize : Sequelize =  (node_env === 'dev') ? new Sequelize({
         dialect: 'sqlite',
         storage: './tridinetdb.sqlite',
-    }) : (node_env === 'production') ?
-    new Sequelize(process.env.DATABASE, process.env.DB_PORT, process.env.DB_PASSWORD, {
+    }) : (node_env === 'prod') ?
+    new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
         dialect: 'mysql'
       }) : null;
 
--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,6 +1,6 @@
-const S3 = require('aws-sdk/clients/s3'); 
-require('dotenv').config();
-const fs = require('fs');
+import S3 from 'aws-sdk/clients/s3';
+import {config} from 'dotenv'
+import fs from 'fs';
 
 const s3 = new S3({
     accessKeyId: process.env.AWS_ACCESS_KEY,
@@ -8,16 +8,14 @@ const s3 = new S3({
     region: process.env.AWS_BUCKET_REGION,
     });
  
-export function uploadFile(file){
+export default function uploadFile(file){
     const fileStream = fs.createReadStream(file);
 
         const params = {
             Bucket: process.env.AWS_BUCKET_NAME,
-            Key: file.name,
-            Body: file.data
+            Key: file.filename,
+            Body: fileStream
         };
 
         return s3.upload(params).promise();
 }
-
-module.exports = uploadFile;
